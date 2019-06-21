a6f2e2054a163f467533ce5faf288a766f7fa48d
Ishmet <ishmetjahan@yahoo.com>
2019-06-21 17:21:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma (str)
  if str == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-   elsif str == str.downcase
+   elsif str == str.downcase || str== str.capitalize
    return "HUH ?! SPEAK UP, SONNY!"
  else
    return "NO, NOT SINCE 1938!"
